da6ec187676fe1aa2945bc35bf286d671d058cd8
ahmada3mar <ahmad.aimar@hyperpay.com>
2024-06-27 15:31:24 +0300

    mobile menu

--- plugins/dir.js
+++ plugins/dir.js
@@ -1,5 +1,10 @@
 export default ({ app }, inject) => {
-    const dir = () =>
-      app.i18n.locales.find((x) => x.code === app.i18n.locale)?.dir;
+    const dir = () =>{
+      const dir = app.i18n.locales.find((x) => x.code === app.i18n.locale)?.dir;
+      document.body.classList.remove("rtl")
+      document.body.classList.remove("ltr")
+      document.body.classList.add(dir)
+      return dir
+      }
       inject('dir', dir);
     };
